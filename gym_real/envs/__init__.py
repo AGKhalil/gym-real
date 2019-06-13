c53f60adbfd4ffdbf5b42d728fccd98d79dec2e8
AGKhalil <elghazalykhalil@gmail.com>
2019-06-13 14:36:03 +0100

    First commit. EVERYTHING

---
+++ gym_real/envs/__init__.py
@@ -0,0 +1,1 @@
+from gym_real.envs.real_env import RealEnv
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='gym_real',
+      version='0.0.1',
+      install_requires=['gym', 'mujoco', 'numpy']  # And any other dependencies foo needs
+)
