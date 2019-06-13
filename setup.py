d9e1ce4bd2bb6d9151dbb51e0cbd7b3190b107d0
AGKhalil <elghazalykhalil@gmail.com>
2019-06-13 15:34:02 +0100

    mujoco dependency removed

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_real',
       version='0.0.1',
-      install_requires=['gym', 'mujoco', 'numpy']  # And any other dependencies foo needs
+      install_requires=['gym', 'numpy']  # And any other dependencies foo needs
 )
